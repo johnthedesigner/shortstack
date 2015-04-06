a8e13cb0fa642dc6078fec10fe2e6e44e9cb0ce2
johnthedesigner <john@johnthedesigner.com>
2015-04-06 08:52:51 -0400

    Working on angular controller for schema and DB interactions

--- app/scripts/controllers/list.js
+++ app/scripts/controllers/list.js
@@ -11,10 +11,55 @@ angular.module('scorecardApp')
 	.controller('ListCtrl', ['$scope', '$http',
 	function ($scope, $http) {
 		
-		var dbJson = 'data/db_stg.json';
+		var dbStg = 'data/db_stg.json';
+		var dbSchema = 'data/db_schema.json';
 		
-	    $http.get(dbJson).success(function(data) {
-	        // Return Table Data
-	        $scope.table = data;
+	    $http.get(dbSchema).success(function(data) {
+	        // Return DB schema
+	        $scope.schema = data;
 	    });
-	}])
+	    
+	    $http.get(dbStg).success(function(data) {
+	        // Return Staging DB
+	        $scope.db = data;
+			
+			$scope.addObject = function(model) {
+				// Define objects if undefined
+				if( $scope.db[model] === undefined ){
+					$scope.db = {};
+					if( $scope.db[model] === undefined ){
+						$scope.db[model] = {};
+					}
+				}
+				var objects = $scope.db[model];
+				var fields = $scope.schema.objects[model].fields;
+				var scaffold = {};
+				for( var i=0; i<fields.length; i++ ){
+					scaffold[fields[i].name] = null;
+				}
+				objects[objects.length] = scaffold;
+				console.log($scope);
+			}
+		
+	    });
+	    
+	    $scope.saveDB = function(){
+			
+			var jsonData = $scope.db;
+			jsonData.mode = "update";
+			
+			$.ajax({
+		        type: "GET",
+		        url: "http://jtd.co/shortstack/dbtest.php",
+		        data: jsonData,
+		        contentType: "text/json; charset=utf-8",
+		        dataType: "json",
+		        success: function(data){console.log(data);},
+		        failure: function(errMsg) {
+		            alert(errMsg);
+		        }
+			});
+			
+	    }
+
+	}]);
